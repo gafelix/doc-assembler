6b987c42f988c14e05984e053ca31db744d085a6
Gabriel Felix de Souza Lopes <gabriel.felix@looplex.com.br>
2021-12-06 12:19:35 -0300

    Atualiza regras de desserialização do Document

--- src/main/java/my/project/docassembler/model/Document.java
+++ src/main/java/my/project/docassembler/model/Document.java
@@ -1,6 +1,9 @@
 package my.project.docassembler.model;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.WRAPPER_OBJECT)
 @JsonSubTypes({@JsonSubTypes.Type(value = Inner.class, name = "inner"),
                 @JsonSubTypes.Type(value = Leaf.class, name = "leaf")})
 public interface Document {
