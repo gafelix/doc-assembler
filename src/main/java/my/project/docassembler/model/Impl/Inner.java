a5cb72bb2f147cdca263df395c82804f34e3331e
gafelix <gabriel.felix@looplex.com.br>
2021-12-07 11:41:45 -0300

    Remove imports obsoletos

--- src/main/java/my/project/docassembler/model/Impl/Inner.java
+++ src/main/java/my/project/docassembler/model/Impl/Inner.java
@@ -1,6 +1,5 @@
 package my.project.docassembler.model.Impl;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 import my.project.docassembler.model.Document;
--- src/main/java/my/project/docassembler/service/Impl/DocumentServiceImpl.java
+++ src/main/java/my/project/docassembler/service/Impl/DocumentServiceImpl.java
@@ -2,9 +2,7 @@ package my.project.docassembler.service.Impl;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import my.project.docassembler.bean.SlugifyBean;
 import my.project.docassembler.model.Document;
-import my.project.docassembler.repository.DocumentRepository;
 import my.project.docassembler.service.DocumentService;
 import org.springframework.stereotype.Service;
 
