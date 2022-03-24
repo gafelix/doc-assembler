232447b94cc3f833fec1c672f0e04ec8b381f1de
gafelix <gabriel.felix@looplex.com.br>
2022-03-24 10:25:25 -0300

    Add LoggingAspect class

--- src/main/java/br/com/looplex/docassembler/exceptions/LoggingAspect.java
+++ src/main/java/br/com/looplex/docassembler/exceptions/LoggingAspect.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -11,7 +12,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* br.com.looplex.docassembler.exceptions.handler..*(..)) && args(exception)")
+    @Pointcut("execution(* br.com.looplex.docassembler.exceptions.handler..*(..)) && args(exception)")
+    public void logExceptionHandlers(Exception exception) {}
+
+    @Before("logExceptionHandlers(exception)")
     public void logBefore(JoinPoint joinPoint, Exception exception) {
         log.error(exception.getMessage());
     }
